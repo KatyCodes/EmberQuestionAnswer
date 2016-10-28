1fc0896da454de1bbc73baf4f098c3feaba3c7e1
Katy Henning <katyannhenning@gmail.com>
2016-10-28 11:04:36 -0700

    add function to add new answers

--- app/models/question.js
+++ app/models/question.js
@@ -4,4 +4,5 @@ export default DS.Model.extend({
   author: DS.attr(),
   content: DS.attr(),
   note: DS.attr(),
+  answers: DS.hasMany('answer', { async: true })
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -13,8 +13,8 @@ export default Ember.Route.extend({
       var newQuestion = this.store.createRecord('question', params);
       newQuestion.save();
       this.transitionTo('index');
-    }
-  },
+    },
+    
     update(question, params) {
       Object.keys(params).forEach(function(key) {
     if(params[key]!==undefined) {
@@ -23,6 +23,12 @@ export default Ember.Route.extend({
       });
       question.save();
       this.transitionTo('index');
-},
+  },
 
+    saveAnswer(params) {
+     var newAnswer = this.store.createRecord('answer', params);
+     newAnswer.save();
+     this.transitionTo('index');
+   }
+  }
 });
