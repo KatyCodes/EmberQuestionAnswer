2c0646f59671308011c6ad0bac78a9bd33c74c57
Katy Henning <katyannhenning@gmail.com>
2016-11-04 10:40:56 -0700

    add helper to show which questions have less than 5 answers

--- app/components/question-detail.js
+++ app/components/question-detail.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  heading: Ember.computed('question.author', 'question.content', function() {
+  questionHeader: Ember.computed('question.author', 'question.content', function() {
    return this.get('question.content') + ' by: ' + this.get('question.author');
  }),
 });
