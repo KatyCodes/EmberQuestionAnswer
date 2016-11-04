62d8f2525caa477643f1c25742693d0ffcb4b34e
Katy Henning <katyannhenning@gmail.com>
2016-11-04 10:19:33 -0700

    add a computed property to the question detail component

--- app/components/question-detail.js
+++ app/components/question-detail.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  heading: Ember.computed('question.author', 'question.content', function() {
+   return this.get('question.content') + ' by: ' + this.get('question.author');
+ }),
 });
