82ecd9d7d8b0f9af196e0557f58ce6eb70a4b638
Katy Henning <katyannhenning@gmail.com>
2016-11-04 12:12:22 -0700

    add service

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,5 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  
+  saveQuestion: Ember.inject.service(),
+  actions: {
+    add(question) {
+      this.get('saveQuestion').add(question);
+    }
+  }
 });
